00b1c118feb574ad3f0b2651ea829dd73ae6402b
Georgii Ishchenko <elsid1993@gmail.com>
2017-05-05 16:59:14 +0300

    Added portfolio page

--- server.js
+++ server.js
@@ -50,6 +50,12 @@ app.get('/about', (req, res) => {
     })
 });
 
+app.get('/projects', (req, res) => {
+    res.render('portfolio.hbs', {
+        pageTitle: 'Portfolio Page',
+    })
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Oops! Some problem here!'
